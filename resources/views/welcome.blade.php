4bc7a35f31ea511ef6634ea9eca2908499b702c5
JariXx2 <korelskiy_2000@mail.ru>
2025-03-05 15:35:29 +0300

    add Controller for page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
     </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+    <body class="">
         <h1>Добро пожаловать на наш сайт!</h1>
 
     </body>
